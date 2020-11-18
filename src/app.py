c63ae607d7d4addf8c4cf7caa3dcafb48d493edd
Lucas Oliveira <olivmath@protonmail.com>
2020-11-18 06:50:56 -0300

    banco de dados integrado com api rest

---
+++ src/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+from json import loads, dump
+from db import *
+
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+	return 'ok!'
+
+
+@app.route('/rank')
+def get_rank():
+	return select_all()
+
+
+def get_data(received):
+	data = {
+		'user': received['user'],
+		'color': '#' + received['color'],
+		'score': int(received['score'])
+	}
+	return data
+
+
+@app.route('/new-user')
+def new_user():
+	""" 
+		Exemplo de requisição -> user: Lucas, color: #FF0, score: 9991
+		http://127.0.0.1:5000/new-user?user=Lucas&color=FF0&score=9991
+	"""
+	new = get_data(request.args)
+	rank = select_all()
+	rank['rank'].append(new)
+	insert(new['user'], new['color'], new['score'])
+	return f'{new}<br> Adiconado com Sucesso!'
+
+
+@app.route('/update')
+def update():
+	""" 
+		Exemplo de requisição -> user: Lucas, color: #FF0, score: +9
+		http://127.0.0.1:5000/update?user=Lucas&color=FF0&score=9
+	"""
+	query = get_data(request.args)
+	# consulta dados
+	score = select_single(query['user'])[0][2]
+	# adiciona
+	query['score'] = int(query['score'] + score)
+	# update
+	update_db(query)
+
+	if int(request.args['score']) > 0:
+		return f"""<h1>{query['user']} ganhou {request.args['score']} pontos!<h1>
+					<br> :)
+				<h2>Pontuação total: {query['score']}<h2>"""
+	else:
+		return f"""<h1>{query['user']} perdeu {request.args['score']} pontos!<h1>
+					<br> :(
+				   <h2>Pontuação total: {query['score']}<h2>"""
+	
+	
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
--- src/db.py
+++ src/db.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 
-conn = sql.connect("Ranking.db")
+conn = sql.connect("Ranking.db", check_same_thread=False)
 cursor = conn.cursor()
 
 
@@ -13,28 +13,67 @@ def create_db():
     return True
 
 
-# pega os dados
-def select():
-    command = "SELECT * FROM rank"
+# pega um dado
+def select_single(query):
+    command = f"SELECT * FROM rank WHERE name == '{query}'"
     cursor.execute(command)
     data = cursor.fetchall()
     conn.commit()
-    return [print(i) for i in data]
+
+    return data
 
 
-# insere dados
+# pega todos dados
+def select_all():
+    command = "SELECT * FROM rank ORDER BY score desc"
+    cursor.execute(command)
+    data = cursor.fetchall()
+    conn.commit()
+
+    rank= {'rank': []}
+    # compresão de dicionario
+    for i in [i for i in data]:
+        new = {'user': i[0], 'color': i[1], 'score': i[2]}
+        rank['rank'].append(new)
+
+    return rank
+
+
+# insere dados novos
 def insert(*args):
     """
-        data tem que um lista do tipo:
-        [nome, cor, score]
+        ex:
+        nome, cor, score
     """
     command = "INSERT INTO rank VALUES (?, ?, ?)"
     cursor.executemany(command, [args])
     conn.commit()
-    return select()
+    return True
 
 
-create_db()
+# faz update de dados
+def update_db(data):
+    command = f"UPDATE rank SET score = {data['score']}  where name == '{data['user']}'"
+    cursor.execute(command)
+    conn.commit()
+    return True
 
 
-insert('Lucas', '#000000', 12000)
+# rank = {'rank':[
+# 		{'user': 'Saymon',    'color': '#FF1616', 'score': 1600},
+# 		{'user': 'Henrico',   'color': '#FF7637', 'score': 1400},
+# 		{'user': 'Israel',    'color': '#FF1616', 'score': 1100},
+# 		{'user': 'Benjamin',  'color': '#F9B11F', 'score': 1100},
+# 		{'user': 'Milena',    'color': '#38B6FF', 'score': 900},
+# 		{'user': 'Gustavo',   'color': '#8C52FF', 'score': 600},
+# 		{'user': 'Jonhana',   'color': '#CF3190', 'score': 600},
+# 		{'user': 'Felipe',    'color': '#2F05FF', 'score': 500},
+# 		{'user': 'Alexandre', 'color': '#000000', 'score': 500},
+# 		{'user': 'Luigi',     'color': '#008037', 'score': 400},
+# 		{'user': 'Daniel',    'color': '#B05A42', 'score': 400},
+# 		{'user': 'Daniel G',  'color': '#50AD29', 'score': 400},
+# 		{'user': 'Vitor',     'color': '#782A89', 'score': 400},
+# 		{'user': 'Luis',      'color': '#FF7637', 'score': 200},
+# 		{'user': 'Luca',      'color': '#545454', 'score': 100},
+# 	]
+# }
