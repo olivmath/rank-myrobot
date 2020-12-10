09a3a6105a69a0ba747d1d598a54696cc50a76bc
Lucas Oliveira <olivmath@protonmail.com>
2020-12-10 07:58:06 -0300

    update quase pronto para deploy

--- src/app.py
+++ src/app.py
@@ -1,21 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, url_for, request, render_template
 from json import loads, dump
 from db import *
 from flask_cors import CORS
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 CORS(app)
-
-@app.route('/')
-def home():
-	return jsonify({'status':'ok!'})
-
-
-@app.route('/rank')
-def get_rank():
-	return select_all()
-
 
 def get_data(received):
 	data = {
@@ -24,6 +14,16 @@ def get_data(received):
 		'score': int(received['score'])
 	}
 	return data
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/rank')
+def get_rank():
+	rank = select_all()
+	return render_template('teste.html', rank=rank)
 
 
 @app.route('/new-user')
--- src/db.py
+++ src/db.py
@@ -60,21 +60,3 @@ def update_db(data):
 
 
 
-# rank = {'rank':[
-# 		{'user': 'Saymon',    'color': '#FF1616', 'score': 1600},
-# 		{'user': 'Henrico',   'color': '#FF7637', 'score': 1400},
-# 		{'user': 'Israel',    'color': '#FF1616', 'score': 1100},
-# 		{'user': 'Benjamin',  'color': '#F9B11F', 'score': 1100},
-# 		{'user': 'Milena',    'color': '#38B6FF', 'score': 900},
-# 		{'user': 'Gustavo',   'color': '#8C52FF', 'score': 600},
-# 		{'user': 'Jonhana',   'color': '#CF3190', 'score': 600},
-# 		{'user': 'Felipe',    'color': '#2F05FF', 'score': 500},
-# 		{'user': 'Alexandre', 'color': '#000000', 'score': 500},
-# 		{'user': 'Luigi',     'color': '#008037', 'score': 400},
-# 		{'user': 'Daniel',    'color': '#B05A42', 'score': 400},
-# 		{'user': 'Daniel G',  'color': '#50AD29', 'score': 400},
-# 		{'user': 'Vitor',     'color': '#782A89', 'score': 400},
-# 		{'user': 'Luis',      'color': '#FF7637', 'score': 200},
-# 		{'user': 'Luca',      'color': '#545454', 'score': 100},
-# 	]
-# }
