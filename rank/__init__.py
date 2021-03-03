8e31b813d42487adbaaf5c5c9dbf0ef6d6c1aa98
Lucas Oliveira <olivmath@protonmail.com>
2021-03-03 01:15:20 -0300

    rest -> restful

--- rank/__init__.py
+++ rank/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_restful import Resource, Api
 from flask_cors import CORS
 
+
 app = Flask(__name__, static_url_path='/static')
+api = Api(app)
 CORS(app)
+
+
 from rank import view
--- rank/view.py
+++ rank/view.py
@@ -2,57 +2,60 @@ from flask import jsonify, url_for, request, render_template, redirect
 from json import loads, dump
 from rank.work import *
 from os import getenv
-from rank import app, settings
+from rank import settings, api, Resource
 
 user = getenv("LOGIN")
 pwd = getenv("PASSWORD")
-print(user, pwd)
+
+class Rank(Resource):
+	def get(self):
+		rank = select_all()
+		return rank
 
 
-@app.route('/')
-def rank():
-	rank = select_all()
-	return render_template('rank.html', rank=rank)
+class Login(Resource):
+	def get(self):
+		return render_template('login.html')
 
 
-@app.route('/login')
-def login():
-	return render_template('login.html')
+class Verify(Resource):
+	def post(self):
+		form = dict(request.form)
+		if form['user'] == user and form['pwd'] == pwd:
+			return redirect(url_for('edit'))
+		else:
+			return redirect(url_for('rank'))
+
+class Edit(Resource):
+	def get(self):
+		return render_template('edit.html')
+
+class Update_user(Resource):
+	def post(self):
+		form = dict(request.form)
+		print(f"Usuario {form['user']} Atualizado")
+		update_db(name=form['user'], score=int(form['score']))
+		return render_template('edit.html')
+
+class Insert_user(Resource):
+	def post(self):
+		form = dict(request.form)
+		print(f"Usuario {form['user']} inserido")
+		insert_db(form['user'], form['color'], int(form['score']))
+		return render_template('edit.html')
+
+class Delete_user(Resource):
+	def post(self):
+		form = dict(request.form)
+		print(f"Usuario {form['user']} Deletado")
+		delete_db(form['user'])
+		return render_template('edit.html')
 
 
-@app.route('/verify', methods=['POST'])
-def verify():
-	form = dict(request.form)
-	if form['user'] == user and form['pwd'] == pwd:
-		return redirect(url_for('edit'))
-	else:
-		return redirect(url_for('rank'))
-
-@app.route('/edit')
-def edit():
-	return render_template('edit.html')
-
-
-@app.route('/update_user', methods=['POST'])
-def update_user():
-	form = dict(request.form)
-	print(f"Usuario {form['user']} Atualizado")
-	update_db(name=form['user'], score=int(form['score']))
-	return render_template('edit.html')
-
-
-@app.route('/insert_user', methods=['POST'])
-def insert_user():
-	form = dict(request.form)
-	print(f"Usuario {form['user']} inserido")
-	insert_db(form['user'], form['color'], int(form['score']))
-	return render_template('edit.html')
-
-
-@app.route('/delete_user', methods=['POST'])
-def delete_user():
-	form = dict(request.form)
-	print(f"Usuario {form['user']} Deletado")
-	delete_db(form['user'])
-	return render_template('edit.html')
-
+api.add_resource(Rank, '/')
+api.add_resource(Login, '/login')
+api.add_resource(Verify, '/verify')
+api.add_resource(Edit, '/edit')
+api.add_resource(Update_user, '/update_user')
+api.add_resource(Insert_user, '/insert_user')
+api.add_resource(Delete_user, '/delete_user')
